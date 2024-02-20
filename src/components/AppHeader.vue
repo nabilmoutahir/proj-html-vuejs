d4be5ad830c5002e8263f71adbcad0e1f950cc30
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-20 01:40:06 +0100

    add header structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,9 +4,18 @@
 
         data() {
             return {
-                
+                headerButtons: [
+                    {
+                        link: 'link',
+                        icon: 'icon'
+                    },
+                    {
+                        link: 'link',
+                        icon: 'icon'
+                    }
+                ],
 
-
+                slogan:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, modi!'
 
             }
         },       
@@ -19,8 +28,57 @@
 
 <template>
 
+    <header>
+        <div class="d-flex justify-content-between py-2 px-5">
+            <div>
+                LOGO
+            </div>
+
+            <div>
+                <div>
+                    EMOJI
+                </div>
+
+                <div>
+                    EMOJI
+                </div>
+            </div>
+        </div>
+    
+        <div>
+            <div>
+                <div>
+                    SQUARE
+                </div>
+
+                <div>
+                   SQUARE 
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <div>
+                AVADA FREELANCE
+            </div>
+
+            <div>
+                ART DIRECTOR
+            </div>
+
+            <div>
+                {{ slogan }}
+            </div>
+        </div>
+
+    </header>
+
 </template>
 
 <style lang="scss" scoped>
+
+    header{
+        height: 100vh;
+    }
 
 </style>
