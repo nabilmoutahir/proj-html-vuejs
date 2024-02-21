7d4788934ebdef8ebfa1afa82fd5e036c425cef1
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-21 04:52:27 +0100

    add appMain component

---
+++ src/components/AppAbout.vue
@@ -0,0 +1,63 @@
+<script>
+
+    import { store } from '../store/store';
+
+    export default {
+        data() {
+
+            return {
+
+                store,
+
+            }
+
+        },
+
+    }
+
+</script>
+
+<template>
+    <main>
+    
+        <div class="text-center"> 
+            <h2>
+                About Me
+            </h2>
+
+            <div>
+                UI & UX EXPERT
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-center">
+            <div class="col-3">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eveniet, blanditiis labore cumque culpa dignissimos neque illo asperiores modi iure laudantium tenetur porro tempora eos quos hic ipsa iste nihil.
+                </p>
+
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque nulla accusamus vero cum earum velit, eius quis provident delectus, distinctio voluptas porro. Non quidem fuga sit voluptatem accusantium sint molestiae optio amet ducimus veniam? Aliquid, quis. Quisquam, doloribus laudantium delectus ipsam perferendis provident officia similique voluptatem odio nisi mollitia nam!
+                </p>
+            </div>
+
+            <div class="col-3">
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque nulla accusamus vero cum earum velit, eius quis provident delectus, distinctio voluptas porro. Non quidem fuga sit voluptatem accusantium sint molestiae optio amet ducimus veniam? Aliquid, quis. Quisquam, doloribus laudantium delectus ipsam perferendis provident officia similique voluptatem odio nisi mollitia nam!
+                </p>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eveniet, blanditiis labore cumque culpa dignissimos neque illo asperiores modi iure laudantium tenetur porro tempora eos quos hic ipsa iste nihil.
+                </p>
+            </div>
+        </div>
+
+        <link rel="stylesheet" href="">
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
