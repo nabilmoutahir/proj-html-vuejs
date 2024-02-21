6db7ccd9f4ab9a0c4fe44bb921d34d8844aade8e
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-21 19:16:47 +0100

    add and style footer component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,67 @@
-<script setup>
+<script>
+
+    export default {
+
+        props:{
+            img: String,
+            url: String,
+            socials:String,
+        },
+
+
+
+    };
 
 </script>
 
 <template>
 
+    <footer>
+        <section class="py-5">
+
+            <div class="text-center pt-5">
+                <a :href="url">
+                    <img :src="img" alt="">
+                </a>
+            </div>
+
+            <div class="text-center py-4">
+                <span>Copyright 2012 - 2020 |</span>
+                <span> Avada Theme by <a :href="url">Theme Fusion</a> |</span>
+                <span> All Rights Reserved |</span>
+                <span> Powered by <a :href="url">WordPress</a></span>
+            </div>
+
+            <div class="d-flex justify-content-center align-content-center pb-5">
+                <div class="px-3" v-for="social in socials">
+                    <a :href="url">
+                        <i :class="social"></i>
+                    </a>
+                </div>
+            </div>
+
+        </section>
+    </footer>
+
 </template>
 
 <style lang="scss" scoped>
+
+    
+    footer{
+        background-color: #1d2028;
+        color: #7887a9;
+        
+        a{
+            color: white;
+            text-decoration: none;
+        }
+
+        i{
+            color: white;
+            overflow: hidden;
+            font-size: xx-large;
+        }
+    }
 
 </style>
---
+++ src/components/AppFooterComp.vue
@@ -0,0 +1,48 @@
+<script>
+
+    import AppFooter from './AppFooter.vue';
+
+export default {
+
+    data (){
+        return{
+            footerItem: {
+                img:'/1logo/avada-footer-logo.png',
+                url:'#',
+            },
+
+            socials:[
+                'fa-brands fa-square-facebook',
+                'fa-brands fa-square-twitter',
+                'fa-brands fa-square-instagram',
+                'fa-brands fa-square-youtube'
+            ],
+
+        }
+
+    },
+
+    components: {
+        AppFooter
+    }
+
+};
+
+</script>
+
+<template>
+
+    <app-footer
+        :img="footerItem.img"
+        :url="footerItem.url"
+        :socials="socials"
+    
+    />
+
+</template>
+
+<style lang="scss" scoped>
+
+
+
+</style>
