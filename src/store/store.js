727f99d2f43f041edcdb9f00ab0d91919fd8c8fd
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-21 12:15:01 +0100

    add elements

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ export const store = reactive({
 
     shortText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eveniet, blanditiis labore cumque culpa dignissimos neque illo asperiores modi iure laudantium tenetur porro tempora eos quos hic ipsa iste nihil',
 
-    longText: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque nulla accusamus vero cum earum velit, eius quis provident delectus, distinctio voluptas porro. Non quidem fuga sit voluptatem accusantium sint molestiae optio amet ducimus veniam? Aliquid, quis. Quisquam, doloribus laudantium delectus ipsam perferendis provident officia similique voluptatem odio nisi mollitia nam!',
+    longText: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque nulla accusamus vero cum earum velit, eius quis provident delectus, distinctio voluptas porro. Non quidem fuga sit voluptatem accusantium sint molestiae optio amet ducimus veniam? Aliquid, quis. Quisquam, doloribus laudantium delectus ipsam perferendis provident officia',
 
     
 });
