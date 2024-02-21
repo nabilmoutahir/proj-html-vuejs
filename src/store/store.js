5ba84f1d1d09dd2ac2960020f775bd510332ced7
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-21 04:51:31 +0100

    add store js

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+
+    services: 'WEB - BRANDING - UX/UI',
+
+    shortText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eveniet, blanditiis labore cumque culpa dignissimos neque illo asperiores modi iure laudantium tenetur porro tempora eos quos hic ipsa iste nihil'
+
+});
