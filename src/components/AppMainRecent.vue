8d03edeea81a47fecdaf58aab87b8d069f238186
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-21 12:55:04 +0100

    add component

---
+++ src/components/AppMainRecent.vue
@@ -0,0 +1,27 @@
+<script>
+
+import { store } from '../store/store';
+
+export default {
+    data() {
+
+        return {
+
+            store,
+        }
+
+    },
+
+}
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
